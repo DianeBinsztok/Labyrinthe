307383eb7de3458347b102088d1b4c198bb2d5b4
DianeBinsztok <diane.binsztok@le-campus-numerique.fr>
2022-07-07 14:48:00 +0200

    feat: createLabyrinth(): Start and Goal positions

--- index.js
+++ index.js
@@ -1,44 +1,50 @@
-// I création du tableau
+// I  - création du tableau
+
+// a - Les proportions
 let nbOfColumns = 7;
 let nbOfRows = 6;
 
-// Placement des murs
+// b - Départ-arrivée
+const S = {"x": 0, "y": 0};
+const G = {"x": 4, "y": 2};
+
+// c - Les murs
 const blocks = [{"x": 1, "y": 0}, {"x": 1, "y": 1}, {"x": 1, "y": 2}, {"x": 1, "y": 4}, {"x": 3, "y": 1}, {
     "x": 3,
     "y": 2
 }, {"x": 3, "y": 4}, {"x": 4, "y": 1}, {"x": 4, "y": 4}, {"x": 5, "y": 3}, {"x": 5, "y": 5}, {"x": 6, "y": 1}];
+
 // Mise en place du labyrinthe
-const createLabyrinth = (x, y, blocksTab) => {
+const createLabyrinth = (start, goal, nbOfColumns, nbOfRows, blocksTab) => {
     let labyrinth = [];
     // Nombre de lignes:
-    for (let nbOfRows = 0; nbOfRows < y; nbOfRows++) {
+    for (let y = 0; y < nbOfRows; y++) {
         let row = [];
         // Nombre de colonnes sur chaque ligne:
-        for (let nbOfColumns = 0; nbOfColumns < x; nbOfColumns++) {
-
-            let square = {"x": nbOfColumns, "y": nbOfRows, "clear": true, "visited": false};
-            // si les coordonnées de la case correspondent à celle d'un block
+        for (let x = 0; x < nbOfColumns; x++) {
+            // Nombre de case sur la ligne
+            let square = {"x": x, "y": y, "clear": true, "visited": false};
+            // a - Si les coordonnées de la case correspondent à celles du départ ou du but:
+            if (square.x === start.x && square.y === start.y) {
+                square = "S";
+            } else if (square.x === goal.x && square.y === goal.y) {
+                square = "G";
+            }
+            // b - si les coordonnées de la case correspondent à celle d'un block:
             blocksTab.forEach(element => {
                 if ((element.x === square.x) && (element.y === square.y)) {
-                    square.clear = false;
+                    square="######";
+                    //square.clear = false;
                 }
             });
-
-
             row.push(square);
         }
-
-
         labyrinth.push(row);
     }
-
     return labyrinth;
 }
-const labyrinth = createLabyrinth(nbOfColumns, nbOfRows, blocks);
+const labyrinth = createLabyrinth(S, G, nbOfColumns, nbOfRows, blocks);
 
-
-const S = labyrinth[0][0];
-const G = labyrinth[2][4];
 let blocktest = labyrinth[1][1];
 
 console.log("coordonnées de S -> ", S);
